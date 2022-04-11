6be40bc6c40a7bc5073e8a97acd1ce007a2acf8c
himanshudhangar <99574294+himanshudhangar@users.noreply.github.com>
2022-04-11 22:35:42 +0530

    Calculate Gambler Won or Lost

--- src/Gambler/Gambler1.java
+++ src/Gambler/Gambler1.java
@@ -2,12 +2,24 @@ package Gambler;
 
 public class Gambler1 {
 
-	static int stake = 100;
-	static int bet = 1;
-
-	public static void main(String[] args) {
-
-		System.out.println("gambler starts with stake " + stake);
-		System.out.println("Gambler bet per stake " + bet);
-	}
+	static int stake=100;
+		static final int bet=1;
+	    
+		
+			public static void main(String[] args) 
+			{
+				System.out.println("available Stack :" + stake + "$");
+	      			  System.out.println("Person Betting Every Game :" + bet + "$");
+	        
+	       			 int betPrice = (int) Math.floor(Math.random() * 10) % 2;
+			
+	    			  	  if (betPrice == 1) 
+					{
+	       			 	    System.out.println("Won then Get :" + betPrice + "$");
+	    			   
+				 	 } else 
+			          	    System.out.println("loose then Get:" + betPrice + "$");
+	     		   }
+             
+		}
 }
