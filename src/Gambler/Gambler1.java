6227f2ed844835515c86232366b4be0d6a1ef69b
himanshudhangar <99574294+himanshudhangar@users.noreply.github.com>
2022-04-11 22:45:53 +0530

    After 20 day amount won or lost

--- src/Gambler/Gambler1.java
+++ src/Gambler/Gambler1.java
@@ -3,28 +3,33 @@ package Gambler;
 public class Gambler1 {
 
 	public static void main(String[] args) {
+			
+			int stake = 100;
+			int bet = 1;
+			int TotalwinAmt = 0;
 
-		System.out.println("Welcome To Gambling Game");
+			
+		        System.out.println("Person Having Stack :" + stake + "$");
+		        System.out.println("Person Betting Every Game :" + bet + "$");
 
-	        int stake = 100;
-	        int bet = 1;
+		        for(int day=1;day<=20;day++) {
+		        while (stake <= 150 && stake >= 50) {
+		            int betPrice = (int) Math.floor(Math.random() * 10) % 2;
+				    if (betPrice == 1) {
+					System.out.println("Won He Get :" + betPrice + "$");
+					stake++;
+					System.out.println("Total stake :" + stake);
+				    } else {
+					System.out.println("loose He Get:" + betPrice + "$");
+					stake--;
+					System.out.println("Total stake :" + stake);
+				    }
+		        }
+				int day_win_amount = stake - 100;
+				System.out.println(" Amount won for day " + day + " is " + day_win_amount);
+				TotalwinAmt = TotalwinAmt + day_win_amount;
 
-	        System.out.println("Person Having Stack :" + stake + "$");
-	        System.out.println("Person Betting Every Game :" + bet + "$");
-
-			while (stake <= 150 && stake >= 50) {
-			    int betPrice = (int) Math.floor(Math.random() * 10) % 2;
-			    if (betPrice == 1) {
-				System.out.println("Won He Get :" + betPrice + "$");
-				stake++;
-				System.out.println("Total stake :" + stake);
-			    } else {
-				System.out.println("loose He Get:" + betPrice + "$");
-				stake--;
-				System.out.println("Total stake :" + stake);
-	            }
-	        }
-	        int TotalStake = stake;
-	        System.out.println("Total Stake For Resign A Day is :" + TotalStake);
-	    }
+				System.out.println("total amount " + TotalwinAmt);
+			}
+		}
 }
