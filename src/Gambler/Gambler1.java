a7d9b63a32387b1185566646d18b3d0b32741d93
himanshudhangar <99574294+himanshudhangar@users.noreply.github.com>
2022-04-11 22:41:29 +0530

    total amount won or lost

--- src/Gambler/Gambler1.java
+++ src/Gambler/Gambler1.java
@@ -2,24 +2,29 @@ package Gambler;
 
 public class Gambler1 {
 
-	static int stake=100;
-		static final int bet=1;
-	    
-		
-			public static void main(String[] args) 
-			{
-				System.out.println("available Stack :" + stake + "$");
-	      			  System.out.println("Person Betting Every Game :" + bet + "$");
-	        
-	       			 int betPrice = (int) Math.floor(Math.random() * 10) % 2;
-			
-	    			  	  if (betPrice == 1) 
-					{
-	       			 	    System.out.println("Won then Get :" + betPrice + "$");
-	    			   
-				 	 } else 
-			          	    System.out.println("loose then Get:" + betPrice + "$");
-	     		   }
-             
-		}
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Gambling Game");
+
+	        int stake = 100;
+	        int bet = 1;
+
+	        System.out.println("Person Having Stack :" + stake + "$");
+	        System.out.println("Person Betting Every Game :" + bet + "$");
+
+			while (stake <= 150 && stake >= 50) {
+			    int betPrice = (int) Math.floor(Math.random() * 10) % 2;
+			    if (betPrice == 1) {
+				System.out.println("Won He Get :" + betPrice + "$");
+				stake++;
+				System.out.println("Total stake :" + stake);
+			    } else {
+				System.out.println("loose He Get:" + betPrice + "$");
+				stake--;
+				System.out.println("Total stake :" + stake);
+	            }
+	        }
+	        int TotalStake = stake;
+	        System.out.println("Total Stake For Resign A Day is :" + TotalStake);
+	    }
 }
