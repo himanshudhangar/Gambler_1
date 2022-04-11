70142292cca176d29f19990efaa5d8b5041d5558
himanshudhangar <99574294+himanshudhangar@users.noreply.github.com>
2022-04-11 22:18:20 +0530

    Starting initialize

---
+++ src/Gambler/Gambler1.java
@@ -0,0 +1,13 @@
+package Gambler;
+
+public class Gambler1 {
+
+	static int stake = 100;
+	static int bet = 1;
+
+	public static void main(String[] args) {
+
+		System.out.println("gambler starts with stake " + stake);
+		System.out.println("Gambler bet per stake " + bet);
+	}
+}
--- src/Gambler/Gambler_Basic.java
+++
@@ -1,9 +0,0 @@
-package Gambler;
-
-public class Gambler_Basic {
-
-	public static void main(String[] args) {
-		System.out.println("Hello Word");
-	}
-
-}
